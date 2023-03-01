05675ca0e8461c7c12eda33b3bc73b586e6455f1
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-03-01 09:41:05 -0500

    bryansito
    abuelito uwu

--- ProyectoM4A/src/main/java/com/ista/springboot/app/controller/CategoriaRestController.java
+++ ProyectoM4A/src/main/java/com/ista/springboot/app/controller/CategoriaRestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ista.springboot.app.models.entity.Categoria;
 import com.ista.springboot.app.models.services.ICategoriaService;
-
+//pene psicologico
 @RestController
 @RequestMapping("/api")
 public class CategoriaRestController {
