2a37f07fb2850d73b390634340c504d98527a61b
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-03-04 23:13:39 -0500

    update ventaservicesimpl
    xdxdxd

--- ProyectoM4A/src/main/java/com/ista/springboot/app/models/services/VentaServiceImpl.java
+++ ProyectoM4A/src/main/java/com/ista/springboot/app/models/services/VentaServiceImpl.java
@@ -6,54 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ista.springboot.app.models.dao.IUsuarioDao;
-import com.ista.springboot.app.models.entity.Usuario;
-import com.ista.springboot.app.models.services.I.IUsuarioService;
+import com.ista.springboot.app.models.dao.IVentaDao;
+import com.ista.springboot.app.models.entity.Venta;
+import com.ista.springboot.app.models.services.I.IVentaService;
 
 @Service
-public class VentaServiceImpl implements IUsuarioService{
+public class VentaServiceImpl implements IVentaService{
 
 	@Autowired
-	private IUsuarioDao dao;
-	
-	@Override
-	@Transactional(readOnly = true)
-	public List<Usuario> findAll() {
-		return (List<Usuario>)dao.findAll();
-	}
-
-	@Override
-	public Usuario save(Usuario usuario) {
-		return dao.save(usuario);
-	}
+	private IVentaDao dao;
 
 	@Override
 	@Transactional(readOnly = true)
-	public Usuario findById(Long Id) {
+	public List<Venta> findAll() {
+		// TODO Auto-generated method stub
+		return (List<Venta>)dao.findAll();
+	}
+
+	@Override
+	public Venta save(Venta venta) {
+		// TODO Auto-generated method stub
+		return dao.save(venta);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Venta findById(Long Id) {
+
 		return dao.findById(Id).orElse(null);
 	}
 
 	@Override
 	public void delete(Long Id) {
+	
 		dao.deleteById(Id);
 	}
-
-	/**
-	 * 
-	 * REVISAR OCACIONES PROBLEMAS 001
-	 * 
-	 * 
-	 */
-	@Override
-	public Usuario findByUserPass(String usu_nombreUsuario, String contrasena) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Boolean existByUser(String usu_nombreUsuario) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
